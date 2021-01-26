be2df0de035a03a38c96932fce143bff75041d53
SempaiEcchi <minecraftm665@gmail.com>
2021-01-26 20:08:43 +0100

    skippable test

--- android/src/main/java/com/jhomlala/better_player/BetterPlayer.java
+++ android/src/main/java/com/jhomlala/better_player/BetterPlayer.java
@@ -111,7 +111,7 @@ final class BetterPlayer {
         this.textureEntry = textureEntry;
         trackSelector = new DefaultTrackSelector(context);
         playerView = new PlayerView(context);
-        adsLoader = new ImaAdsLoader(context, Uri.parse("https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="));
+        adsLoader = new ImaAdsLoader(context, Uri.parse("https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="));
 
 
         exoPlayer = new SimpleExoPlayer.Builder(context).setTrackSelector(trackSelector).build();
